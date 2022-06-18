afb9299743f14da427d437ba5561d9506fbbdb03
dizzystem <sohzq@yahoo.com.sg>
2022-06-18 12:29:29 +0800

    Template from Spring Initializr

---
+++ src/test/java/com/dizzystem/airlab/AirLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dizzystem.airlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
